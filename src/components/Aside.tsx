8f4f7f7070b5a3370886e177c06fa7665120dd49
Yaroslav Shilov <workyarik@gmail.com>
2023-10-26 21:55:29 +0400

    bug with refresh

---
+++ src/components/Aside.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Search } from './Search'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Aside = () => (
+  <StyledAside>
+    <StyledLogo to="/">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+        />
+      </svg>
+      <span>Articlefy</span>
+    </StyledLogo>
+    <Search />
+    <nav>
+      <StyledList>
+        <li>
+          <Link to="/add-article">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>Add new</span>
+          </Link>
+        </li>
+      </StyledList>
+    </nav>
+  </StyledAside>
+)
+
+const StyledAside = styled.aside`
+  flex: 0 0 240px;
+  padding-top: 38px;
+  text-align: center;
+`
+
+const StyledList = styled.ul`
+  padding: 0;
+  list-style: none;
+
+  li {
+    display: flex;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+
+    font: 700 18px/17px 'EB Garamond', serif;
+    color: black;
+
+    text-decoration: none;
+
+    &:hover svg {
+      color: red;
+    }
+  }
+
+  svg {
+    flex: 0 0 24px;
+    color: tomato;
+
+    transition: all 0.5s ease-in-out;
+  }
+
+  span {
+    flex: 0 0 auto;
+    padding-left: 10px;
+  }
+`
+
+const StyledLogo = styled(Link)`
+  display: inline-block;
+  margin-bottom: 27px;
+  color: black;
+  text-align: center;
+  text-decoration: none;
+
+  &:hover svg {
+    color: red;
+  }
+
+  span {
+    font-weight: 700;
+    font-size: 38px;
+  }
+
+  svg {
+    max-width: 124px;
+    margin-bottom: -2px;
+    color: tomato;
+
+    transition: all 0.5s ease-in-out;
+  }
+`
---
+++ src/pages/Home.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+import { Article, ArticleType } from '../../components/Article';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { fetchPartArticles, sortArticles } from '../../store/articlesSlice';
+import styled from 'styled-components';
+import { Loader } from '../../components/Loader';
+
+export const Home = () => {
+  const [offset, setOffset] = useState(0);
+  const [sort, setSort] = useState<'new' | 'old' | ''>('');
+  const { pageArticles, loading, sorting, error } = useAppSelector<{
+    pageArticles: { list: ArticleType[] | null; hasMore: boolean };
+    loading: boolean;
+    sorting: boolean;
+    error: string | null;
+  }>((state) => state.articles);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    //@ts-ignore
+    dispatch(fetchPartArticles(offset));
+  }, [offset, dispatch]);
+
+  const btnHandler = (type: 'prev' | 'next') => () => {
+    setOffset(type === 'prev' ? offset - 4 : offset + 4);
+  };
+
+  const sortHandler = (type: 'new' | 'old') => () => {
+    setSort(type);
+    //@ts-ignore
+    dispatch(sortArticles(type));
+  };
+
+  if (error) return <h2 style={{ textAlign: 'center' }}>{error}</h2>;
+
+  if (!pageArticles.list) return <Loader />;
+
+  return (
+    <React.Fragment>
+      <StyledSort>
+        <p>Sort: </p>
+        <button onClick={sortHandler('new')} disabled={sort === 'new'}>
+          New
+        </button>
+        <button onClick={sortHandler('old')} disabled={sort === 'old'}>
+          Old
+        </button>
+      </StyledSort>
+
+      {sorting && <Loader />}
+
+      {!sorting && (
+        <React.Fragment>
+          {pageArticles.list.map((article) => (
+            <StyledArticle key={article.id} {...article} text="" />
+          ))}
+
+          {loading ? (
+            <Loader />
+          ) : (
+            <StyledButtons>
+              <button onClick={btnHandler('prev')} disabled={offset === 0}>
+                Prev
+              </button>
+              <button onClick={btnHandler('next')} disabled={!pageArticles.hasMore}>
+                Next
+              </button>
+            </StyledButtons>
+          )}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
+
+const StyledArticle = styled(Article)`
+  margin-bottom: 60px;
+`;
+
+const StyledButtons = styled.div`
+  margin-top: 40px;
+
+  button {
+    min-width: 100px;
+    margin-right: 20px;
+    padding: 10px;
+
+    text-align: center;
+
+    background-color: transparent;
+    border: 1px solid #d9d9d9;
+    border-radius: 4px;
+    transition: all 0.5s ease-in-out;
+
+    cursor: pointer;
+    outline: 0;
+
+    &:disabled {
+      cursor: default;
+    }
+
+    &:hover:not(:disabled) {
+      border-color: tomato;
+    }
+
+    &:focus,
+    &:active,
+    &:focus-within {
+      &:not(:disabled) {
+        border-color: red;
+      }
+    }
+  }
+`;
+
+const StyledSort = styled(StyledButtons)`
+  display: flex;
+  align-items: center;
+  margin: 0 0 40px;
+
+  p {
+    margin: 0 16px 0 0;
+    font-weight: 700;
+    font-size: 20px;
+  }
+
+  button {
+    height: auto;
+  }
+`;
