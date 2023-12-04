6b39fa497ac78704c2c130d4908bc10f280fc30a
Yaroslav Shilov <workyarik@gmail.com>
2023-12-04 23:06:27 +0400

    Remove imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { lazy } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, createHashRouter, HashRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
